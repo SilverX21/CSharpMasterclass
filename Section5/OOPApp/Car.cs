fc3b1ff2f4fad071b3cc566dc1c4d590c2e28951
Nuno Araújo <nuno.araujo@itsector.pt>
2024-08-07 18:06:28 +0100

    first step into oop

---
+++ Section5/OOPApp/Car.cs
@@ -0,0 +1,18 @@
+﻿namespace OOPApp;
+
+internal class Car
+{
+	private string _model = "";
+	private string _brand = "";
+
+	public string Model { get; set; }
+	public string Brand { get; set; }
+
+	public Car(string model, string brand)
+	{
+		_model = model;
+		_brand = brand;
+
+		Console.WriteLine($"An {_brand} of the model {_model} has been created!");
+	}
+}
---
+++ Section5/OOPApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace OOPApp;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Car audi = new Car("A3", "Audi");
+		Car bmw = new Car("i7", "BMW");
+		Console.WriteLine("Hello, World!");
+	}
+}
